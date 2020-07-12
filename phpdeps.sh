87f8c46dcb946911fb1ceb90295fdd3903f93fff
jorgemanzo <jmanzo203689@gmail.com>
2020-07-11 19:35:28 -0700

    added mysql php driver to phpdeps

--- phpdeps.sh
+++ phpdeps.sh
@@ -4,6 +4,6 @@ printf "\n\n#########################\n"
 printf "# Installing some php deps...........\n"
 printf "#########################\n\n"
 
-apt install php7.4-{zip,mbstring,dom}
+apt install php7.4-{zip,mbstring,dom,mysql}
 
 
