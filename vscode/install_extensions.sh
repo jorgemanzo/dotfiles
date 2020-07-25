b59ca8b0a163808ef17987b653f139d41ce67d10
jorgemanzo <jmanzo203689@gmail.com>
2020-07-25 12:46:43 -0700

    Revert "Updated vscode dots for flatpak"
    This reverts commit d57591deab0ab9c38b6cd661000da94ca15ddd33.

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -3,5 +3,5 @@
 printf "Installing extensions........\n"
 
 while read extension; do
-    flatpak run com.visualstudio.code --install-extension $extension
+    code --install-extension $extension
 done < vscode_extensions
--- vscode/save_extensions.sh
+++ vscode/save_extensions.sh
@@ -2,4 +2,4 @@
 
 printf "Saving installed extensions into vscode_extensions............\n"
 
-flatpak run com.visualstudio.code --list-extensions > vscode_extensions
+code --list-extensions > vscode_extensions
