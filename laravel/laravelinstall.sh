f50f4651a883d286cff5bb236ea5bfe5434bab3e
jorgemanzo <jmanzo203689@gmail.com>
2020-07-29 07:23:47 -0700

    Run composer laravel install as normal user

--- laravel/laravelinstall.sh
+++ laravel/laravelinstall.sh
@@ -4,4 +4,6 @@ printf "\n\n#########################\n"
 printf "# Installing laravel via composer...........\n"
 printf "#########################\n\n"
 
-composer global require laravel/installer
+# This can be ran as non-root.
+# Make sure to check that ~/.composer is not owned by root.
+runuser -l $SUDO_USER -c 'composer global require laravel/installer'
